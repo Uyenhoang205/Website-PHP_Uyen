33ed164e93b950342277d5200e46cc767c9d0bc8
Uyenhoang205 <93367058+Uyenhoang205@users.noreply.github.com>
2021-10-30 00:02:31 +0900

    Update Delete.php

--- Project_Uyen/Delete.php
+++ Project_Uyen/Delete.php
@@ -2,7 +2,7 @@
 if (isset($_GET['id'])) {
     $mobile_id = $_GET['id'];
     $conn = mysqli_connect('localhost', 'root', '');
-    mysqli_select_db($conn, 'quan_ly_dien_thoai');
+    mysqli_select_db($conn, 'mobile_management');
     $sql = "
         DELETE FROM mobile
         WHERE mobile.id = $mobile_id
