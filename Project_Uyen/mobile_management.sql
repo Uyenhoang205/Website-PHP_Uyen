ab1a74b24699815d3db1668979c6614c9d3ddd9d
Uyenhoang205 <93367058+Uyenhoang205@users.noreply.github.com>
2021-11-01 00:55:52 +0900

    Update mobile_management.sql

--- Project_Uyen/mobile_management.sql
+++ Project_Uyen/mobile_management.sql
@@ -1,3 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 22, 2021 at 12:00 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mobile_managementi`
+--
+DROP DATABASE IF EXISTS mobile_management;
+CREATE DATABASE IF NOT EXISTS `mobile_management` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `mobile_management`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mobile`
+--
+
+CREATE TABLE `dien_thoai` (
+  `id` int(11) NOT NULL COMMENT 'mobile id',
+  `mobile_name` varchar(255) NOT NULL COMMENT 'mobile name',
+  `classification` varchar(255) NOT NULL COMMENT 'mobile classification',
+  `price` bigint(20) NOT NULL COMMENT 'price',
+  `image` varchar(255) NOT NULL COMMENT 'link image',
+  `created_datetime` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'created datetime',
+  `updated_datetime` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'updated datetime'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL COMMENT 'user id',
+  `user name` varchar(255) NOT NULL COMMENT 'user name',
+  `password` varchar(20) NOT NULL COMMENT 'user password',
+  `name` varchar(255) NOT NULL COMMENT 'name',
+  `created_datetime` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'created datetime',
+  `updated_datetime` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'updated datetime'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='user information';
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mobile`
+--
+ALTER TABLE `mobile`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `dien_thoai`
+--
+ALTER TABLE `mobile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'mobile id';
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'user id', AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+
 /*
  Navicat Premium Data Transfer
 
